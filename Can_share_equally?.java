78d4e30ace1ac04761bd190c5cdf60458b8adf2c
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-10-25 02:32:00 -0400

    import java.util.*; class CansgsreEqually {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         if(a%2==0 && (a>b || b%2==0))         {             System.out.println("YES");         }         else         {              System.out.println("NO");         }     } }

---
+++ Can_share_equally?.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class CansgsreEqually
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a%2==0 && (a>b || b%2==0))
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+             System.out.println("NO");
+        }
+    }
+}
