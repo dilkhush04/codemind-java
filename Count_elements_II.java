9f284cdec2bc526ba425a7a05ca54fa216f6a11c
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-10-29 09:08:41 -0400

    import java.util.*; class CountElement11 {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int[] arr1=new int[a];         int[] arr2=new int[b];         int[] arr3=new int[b+a];         int i,j,k,p=0,c=0,flag;         for(i=0;i<a;i++)         {            arr1[i]=sc.nextInt();         }          for(i=0;i<b;i++)         {            arr2[i]=sc.nextInt();         }         for(i=0;i<a;i++)         {             for(j=i+1;j<a;j++)             {                 if(arr1[i]==arr1[j])                 {                     for(k=j;k<a-1;k++)                     {                         arr1[k]=arr1[k+1];                     }                     j--;                     a--;                 }
                }
            }
             for(i=0;i<b;i++)
            {
                for(j=i+1;j<b;j++)
                {
                    if(arr2[i]==arr2[j])
                    {
                        for(k=j;k<b-1;k++)
                        {
                            arr2[k]=arr2[k+1];
                        }
                        j--;
                        b--;
                    }
                    
                }
            }
            for(i=0;i<a;i++)
            {
                 flag=0;
                 for(j=0;j<b;j++)
                 {
                      if(arr1[i]==arr2[j])
                      {
                           flag=1;
                      }
                 }
                 if(flag==0)
                 {
                      c++;
                 }
            }
             for(i=0;i<b;i++)
            {
                 flag=0;
                 for(j=0;j<a;j++)
                 {
                      if(arr2[i]==arr1[j])
                      {
                           flag=1;
                      }
                 }
                 if(flag==0)
                 {
                      c++;
                 }
            }
            System.out.println(c);
            
            
        }
    }

---
+++ Count_elements_II.java
@@ -0,0 +1,87 @@
+import java.util.*;
+class CountElement11
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int[] arr1=new int[a];
+        int[] arr2=new int[b];
+        int[] arr3=new int[b+a];
+        int i,j,k,p=0,c=0,flag;
+        for(i=0;i<a;i++)
+        {
+           arr1[i]=sc.nextInt(); 
+        }
+         for(i=0;i<b;i++)
+        {
+           arr2[i]=sc.nextInt(); 
+        }
+        for(i=0;i<a;i++)
+        {
+            for(j=i+1;j<a;j++)
+            {
+                if(arr1[i]==arr1[j])
+                {
+                    for(k=j;k<a-1;k++)
+                    {
+                        arr1[k]=arr1[k+1];
+                    }
+                    j--;
+                    a--;
+                }
+                
+            }
+        }
+         for(i=0;i<b;i++)
+        {
+            for(j=i+1;j<b;j++)
+            {
+                if(arr2[i]==arr2[j])
+                {
+                    for(k=j;k<b-1;k++)
+                    {
+                        arr2[k]=arr2[k+1];
+                    }
+                    j--;
+                    b--;
+                }
+                
+            }
+        }
+        for(i=0;i<a;i++)
+        {
+             flag=0;
+             for(j=0;j<b;j++)
+             {
+                  if(arr1[i]==arr2[j])
+                  {
+                       flag=1;
+                  }
+             }
+             if(flag==0)
+             {
+                  c++;
+             }
+        }
+         for(i=0;i<b;i++)
+        {
+             flag=0;
+             for(j=0;j<a;j++)
+             {
+                  if(arr2[i]==arr1[j])
+                  {
+                       flag=1;
+                  }
+             }
+             if(flag==0)
+             {
+                  c++;
+             }
+        }
+        System.out.println(c);
+        
+        
+    }
+}
