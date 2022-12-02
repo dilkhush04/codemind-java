ebebb65a5a4e78ff149c62852d520cd12fd33c84
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-02 22:01:03 +0530

    import java.util.*; class Fibonacci {
        public static void main(String[] args)
        {
            Scanner sc=new Scanner(System.in);
            int n=sc.nextInt();
            int a=1,b=0,c=0;
            while(n>0)
            {
                System.out.print(c+" ");
               
                c=a+b;
                a=b;
                b=c;
                n--;
                
            }
            
        }
            
           
        }

--- fibonacci.java
+++ fibonacci.java
@@ -1,19 +1,24 @@
 import java.util.*;
 class Fibonacci
 {
+   
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int a=1,b=0,c=0;
-       for(int i=1;i<=n;i++)
-       {
-          System.out.print(c+" ");
-          c=a+b;
-          a=b;
-          b=c;
-          
-       }
+        while(n>0)
+        {
+            System.out.print(c+" ");
+           
+            c=a+b;
+            a=b;
+            b=c;
+            n--;
+            
+        }
+        
+    }
+        
        
     }
-}
