5336b765c7079073beed562a868857bdc1fc07a4
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-02 19:38:18 +0530

    import java.util.*; class UglyNumber {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int  n=sc.nextInt();         double m=Math.pow(n,0.5);         if(m==(int)m)         System.out.println("True");         else         {             System.out.println("False");         }
        }
    }

--- perfect_square_root_or_not.java
+++ perfect_square_root_or_not.java
@@ -5,9 +5,8 @@ class UglyNumber
     {
         Scanner sc=new Scanner(System.in);
         int  n=sc.nextInt();
-        int c=0;
-        double num=Math.pow(n,0.5);
-        if((int)num*(int)num==n)
+        double m=Math.pow(n,0.5);
+        if(m==(int)m)
         System.out.println("True");
         else
         {
