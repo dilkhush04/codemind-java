7eb999d9b5430309c48042978bbe0f03a3cd4fc5
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-02 18:09:45 +0530

    import java.util.*; class CompoundI {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double p=sc.nextDouble();         double r=sc.nextDouble();         double t=sc.nextDouble();         System.out.printf("%.2f",Math.pow((1+0.01*r),t)*p);     } }

--- Compound_interest.java
+++ Compound_interest.java
@@ -1,12 +1,12 @@
 import java.util.*;
-class CompoundInterest
+class CompoundI
 {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        double p=sc.nextInt();
-        double r=sc.nextInt();
-        double t=sc.nextInt();
-        System.out. printf("%.2f",p*Math.pow((1+0.01*r),t));
+        double p=sc.nextDouble();
+        double r=sc.nextDouble();
+        double t=sc.nextDouble();
+        System.out.printf("%.2f",Math.pow((1+0.01*r),t)*p);
     }
 }
