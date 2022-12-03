8e7a43d6736a3a9817e54177ebe215ea06c0f6ca
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-04 00:39:39 +0530

    import java.util.*; class SmallerThenAve {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         int i;         double sum=0;
            int c=0;
            for(i=0;i<n;i++)
            {
                a[i]=sc.nextInt();
            }
            for(i=0;i<n;i++)
            {
                sum=sum+a[i];
            }
          double  s=Math.floor(sum/n);
            for(i=0;i<n;i++)
            {
                if(a[i]<=s)
                {
                    c++;
                }
            }
            System.out.println(c);
        }
    }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class SmallerThenAve
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        int i;
+        double sum=0;
+        
+        int c=0;
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            sum=sum+a[i];
+        }
+      double  s=Math.floor(sum/n);
+        for(i=0;i<n;i++)
+        {
+            if(a[i]<=s)
+            {
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
