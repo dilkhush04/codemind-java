231f4291f9a695096100f2bbbe8c1a96dcb92daf
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-04 01:25:20 +0530

    import java.util.*; class FindTheCapacity {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int s=sc.nextInt();         int t=sc.nextInt();         int b=sc.nextInt();
            System.out.println((s*t*b)+"KB");
        }
    }

---
+++ Find_the_Capacity.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class FindTheCapacity
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int t=sc.nextInt();
+        int b=sc.nextInt();
+        
+        System.out.println((s*t*b)+"KB");
+    }
+}
