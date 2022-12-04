f800d026cfa7b78737186c27a208a6f948ed8a14
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-04 16:13:13 +0530

    import java.util.*; class UniqueEvensum {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         int i,j,k,sum=0;         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             for(j=i+1;j<n;j++)             {                 if(a[i]==a[j])                 {                     for(k=j;k<n-1;k++)                     {                        a[k]=a[k+1] ;                     }                     n--;                     j--;                 }
                }
            }
            for(i=0;i<n;i++)
            {
               if(a[i]%2!=0)
               {
                   sum=sum+a[i];
               }
            }
            System.out.println(sum);
        }
    }

--- unique_odd_elements_sum.java
+++ unique_odd_elements_sum.java
@@ -1,40 +1,39 @@
 import java.util.*;
-class RemoveDuplicateInArrayExample
-{ public static int removeDuplicateElements(int arr[], int n)
-{ if (n==0 || n==1){ return n; } 
-int[] temp = new int[n]; 
-int j = 0; 
-for (int i=0; i<n-1; i++)
-{ if (arr[i] != arr[i+1])
-{ temp[j++] = arr[i];
-} 
-    
-} 
-temp[j++] = arr[n-1]; 
-// Changing original array 
-for (int i=0; i<j; i++){ arr[i] = temp[i]; 
-    
-} 
-return j;
-}
-public static void main (String[] args) 
-{ Scanner sc=new Scanner(System.in);
-int num=sc.nextInt(); 
-int[] arr= new int[num];
-int k; int sum=0; 
-for(k=0;k<num;k++) 
-{ arr[k]=sc.nextInt(); 
-    
-}
-int length = num; 
-length = removeDuplicateElements(arr, length); // printing array elements ;
-for (int i=0; i<length; i++) 
-{ if(arr[i]%2!=0) { sum=sum+arr[i]; 
-    
-} 
-    
-} 
-System.out.println(sum);
-} 
-    
+class UniqueEvensum
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        int i,j,k,sum=0;
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if(a[i]==a[j])
+                {
+                    for(k=j;k<n-1;k++)
+                    {
+                       a[k]=a[k+1] ;
+                    }
+                    n--;
+                    j--;
+                }
+                
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+           if(a[i]%2!=0)
+           {
+               sum=sum+a[i];
+           }
+        }
+        System.out.println(sum);
+    }
 }
