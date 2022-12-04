17607bbda7c3ae24153afd47f7457fc243becafb
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-04 16:26:48 +0530

    import java.util.*; class Minimum{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         int a=sc.nextInt();         int b=sc.nextInt();         int c;         int count=0;         int temp=9999;         for(int i=0;i<n;i++){             if(arr[i]>=a && arr[i]<=b){                 if(arr[i]<=temp){                     temp=arr[i];                 }             }         }         if(temp==9999)             System.out.print("-1");         else             System.out.print(temp);
        }
    }

---
+++ minimum_between_A_and_B.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Minimum{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c;
+        int count=0;
+        int temp=9999;
+        for(int i=0;i<n;i++){
+            if(arr[i]>=a && arr[i]<=b){
+                if(arr[i]<=temp){
+                    temp=arr[i];
+                }
+            }
+        }
+        if(temp==9999)
+            System.out.print("-1");
+        else
+            System.out.print(temp);
+        
+    }
+}
