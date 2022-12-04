94577969f10e28cc207c3363f29ecc0a9bdddf2a
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-04 19:34:17 +0530

    import java.util.Scanner; class CountElementn_Didv_k { public static void main(String[] args) {     int c=0;     Scanner sc=new Scanner(System.in);     int n=sc.nextInt();     int k=sc.nextInt();     int[] array = new int[n];     for(int i=0; i<n; i++)         {             array[i]=sc.nextInt();         }     for (int i=0; i<n; i++)     {         if(array[i]%k==0)             {                 c++;             }     }     System.out.println(c);     } }

--- Count_elements_divisible_by_k.java
+++ Count_elements_divisible_by_k.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;  
-class CountElementDidv_k   
+class CountElementn_Didv_k   
 {  
 public static void main(String[] args)   
 {
-    int n;
     int c=0;
     Scanner sc=new Scanner(System.in);  
-    n=sc.nextInt();
+    int n=sc.nextInt();
     int k=sc.nextInt();
     int[] array = new int[n];
     for(int i=0; i<n; i++)  
