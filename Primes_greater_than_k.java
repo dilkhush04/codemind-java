32d7024f5f0bc42c8d12021eb073c1889a27088e
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-04 17:38:24 +0530

    import java.util.*; class PrimesGreaterrThan_k {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int i,j,flag=0;         int c=0;         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int k=sc.nextInt();
            for(i=0;i<n;i++)
            {
                
               flag=0;
                if(a[i]>1){
                for( j=2;j<=a[i]/2;j++)
                {
                    if(a[i]%j==0)
                    {
                        flag=1;
                        break;
                        
                    }
                    
                }
                if(flag==0)
                    {
                        if(a[i]>=k)
                        {
                            c++;
                        }
                    }
                
            }
            }
            System.out.print(c);
           
        }
    }

--- Primes_greater_than_k.java
+++ Primes_greater_than_k.java
@@ -1,41 +1,45 @@
 import java.util.*;
- class AverageOfPrimes
+class PrimesGreaterrThan_k
 {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int i,j,t,c,flag,k=0;
+        int i,j,flag=0;
+        int c=0;
         int[] a=new int[n];
         for(i=0;i<n;i++)
         {
             a[i]=sc.nextInt();
         }
-        int num=sc.nextInt();
-        for(i=0;i<n;i++){
-            t=a[i];
-   	     if(t<=1)
-     	{
-   		continue;
-	   }
-      flag=0;
-      for(j=2;j<=t/2;++j){ 
-         if(t%j==0){
-            flag=1;
-            break;
-         }
-      }
-      
-      if(t>=num)
-      {
-          if(flag==0)
-          {
-              k++;
-          }
-      }
-        
-   }
+        int k=sc.nextInt();
+            
+        for(i=0;i<n;i++)
+        {
+            
+           flag=0;
+            if(a[i]>1){
+            for( j=2;j<=a[i]/2;j++)
+            {
+                if(a[i]%j==0)
+                {
+                    flag=1;
+                    break;
+                    
+                }
+                
+            }
+            if(flag==0)
+                {
+                    if(a[i]>=k)
+                    {
+                        c++;
+                    }
+                }
+            
+        }
+        }
+        System.out.print(c);
        
-       System.out.println(k);
     }
 }
