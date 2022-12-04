4440d4db9e1d5fa3e079d33fdf015a03cac2b3ea
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-04 16:58:26 +0530

    import java.util.*; class Differencebetween{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int sum=0,sum1=0;         int[] arr=new int[n];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         for(int i=0;i<n/2;i++){             sum+=arr[i];
            }
            for(int i=n/2;i<n;i++){
                sum1+=arr[i];
                
            }
            System.out.println(sum);
            System.out.println(sum1);
            
        }
    }

---
+++ sum_of_first_half_and_second_half_in_an_array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Differencebetween{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0,sum1=0;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n/2;i++){
+            sum+=arr[i];
+            
+        }
+        for(int i=n/2;i<n;i++){
+            sum1+=arr[i];
+            
+        }
+        System.out.println(sum);
+        System.out.println(sum1);
+        
+    }
+}
