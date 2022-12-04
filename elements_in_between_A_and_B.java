16193028ed7769005882be7f5eddb38de6808e26
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-04 16:35:48 +0530

    import java.util.*; class Minimum{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         int a=sc.nextInt();         int b=sc.nextInt();         int c=0;         for(int i=0;i<n;i++)         {             if(arr[i]>=a && arr[i]<=b)             {                 System.out.print(arr[i]+" ");                 c++;             }         }         if(c==0)         {             System.out.println("-1");         }
        }
    }

--- elements_in_between_A_and_B.java
+++ elements_in_between_A_and_B.java
@@ -1,29 +1,27 @@
 import java.util.*;
-class ElementsBetweenAandB
-{
-    public static void main(String[] args)
-    {
+class Minimum{
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int i,c=0;
-        int[] a=new int[n];
-        for(i=0;i<n;i++)
-        {
-            a[i]=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
         }
-        int x=sc.nextInt();
-        int y=sc.nextInt();
-        for(i=0;i<n;i++)
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=0;
+        for(int i=0;i<n;i++)
         {
-            if(a[i]>=x && a[i]<=y)
+            if(arr[i]>=a && arr[i]<=b)
             {
-                System.out.print(a[i]+" ");
+                System.out.print(arr[i]+" ");
                 c++;
             }
         }
         if(c==0)
         {
-           System.out.print("-1"); 
+            System.out.println("-1");
         }
+        
     }
 }
