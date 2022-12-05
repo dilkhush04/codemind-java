55d06fe003f72e1fa6dc7cae2b4c96ba1c83de7a
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-05 18:37:23 +0530

    import java.util.*; class GFG {
        static String minWord = "", maxWord = "";
     
        static void minMaxLengthWords(String input)
        {
              input=input.trim();
            int len = input.length();
            int si = 0, ei = 0;
            int min_length = len, min_start_index = 0,
                  max_length = 0, max_start_index = 0;
     
            while (ei <= len)
            {
                if (ei < len && input.charAt(ei) != ' ')
                {
                    ei++;
                }
                else
                {
                    int curr_length = ei - si;
     
                    if (curr_length < min_length)
                    {
                        min_length = curr_length;
                        min_start_index = si;
                    }
     
                    if (curr_length > max_length)
                    {
                        max_length = curr_length;
                        max_start_index = si;
                    }
                    ei++;
                    si = ei;
                }
            }
            minWord = input.substring(min_start_index, min_start_index + min_length);
            
        }
     
        public static void main(String[] args)
        {
            Scanner sc=new Scanner(System.in);
            String a = sc.nextLine();
     
            minMaxLengthWords(a);
            int num=minWord.length();
            System.out.print(num);
        }
    }

---
+++ minimum_length_of_words.java
@@ -0,0 +1,53 @@
+import java.util.*;
+class GFG
+{
+ 
+    static String minWord = "", maxWord = "";
+ 
+    static void minMaxLengthWords(String input)
+    {
+          input=input.trim();
+        int len = input.length();
+        int si = 0, ei = 0;
+        int min_length = len, min_start_index = 0,
+              max_length = 0, max_start_index = 0;
+ 
+        while (ei <= len)
+        {
+            if (ei < len && input.charAt(ei) != ' ')
+            {
+                ei++;
+            }
+            else
+            {
+                int curr_length = ei - si;
+ 
+                if (curr_length < min_length)
+                {
+                    min_length = curr_length;
+                    min_start_index = si;
+                }
+ 
+                if (curr_length > max_length)
+                {
+                    max_length = curr_length;
+                    max_start_index = si;
+                }
+                ei++;
+                si = ei;
+            }
+        }
+        minWord = input.substring(min_start_index, min_start_index + min_length);
+        
+    }
+ 
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String a = sc.nextLine();
+ 
+        minMaxLengthWords(a);
+        int num=minWord.length();
+        System.out.print(num);
+    }
+}
