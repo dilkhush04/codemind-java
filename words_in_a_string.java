3d8b4029282426d39d3a32d70d359eb790679408
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-05 19:05:48 +0530

    import java.util.*; class Word {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         int i;         String[] a=s.split(" ");         int len=a.length;         System.out.print(len);
        }
    }

---
+++ words_in_a_string.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Word
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int i;
+        String[] a=s.split(" ");
+        int len=a.length;
+        System.out.print(len);
+        
+    }
+}
