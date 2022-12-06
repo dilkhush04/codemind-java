6b4e2471ff9dd933e77dc10d64fa30de0b70cd71
dilkhush04 <99658871+dilkhush04@users.noreply.github.com>
2022-12-06 18:38:36 +0530

    import java.util.*; class Word {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         String[] str=s.split(" ");         for(int i=0;i<str.length;i++)         {             char min='0';             char max='0';             int temp=999;             int temp1=-999;             //System.out.print(str[i]);             for(int j=0;j<str[i].length();j++)             {                 char ch=str[i].charAt(j);                 {                     if((int)ch<temp)                     {                         temp=(int)ch;                         min=ch;                     }                     if((int)ch>temp1)                     {                         temp1=(int)ch;                         max=ch;                     }
                    }
                }
                System.out.print(min+" "+max+" ");
            }
            
        }
    }

---
+++ small_and_large_characters.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Word
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String[] str=s.split(" ");
+        for(int i=0;i<str.length;i++)
+        {
+            char min='0';
+            char max='0';
+            int temp=999;
+            int temp1=-999;
+            //System.out.print(str[i]);
+            for(int j=0;j<str[i].length();j++)
+            {
+                char ch=str[i].charAt(j);
+                {
+                    if((int)ch<temp)
+                    {
+                        temp=(int)ch;
+                        min=ch;
+                    }
+                    if((int)ch>temp1)
+                    {
+                        temp1=(int)ch;
+                        max=ch;
+                    }
+                    
+                }
+            }
+            System.out.print(min+" "+max+" ");
+        }
+        
+    }
+}
